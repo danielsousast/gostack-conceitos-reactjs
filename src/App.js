3335a579c058a4baa7481f8fbf0c8bf2cbc35a50
Daniel Sousa <daniel@Daniels-iMac.local>
2020-06-16 14:59:09 -0300

    Challenge Done

--- src/App.js
+++ src/App.js
@@ -1,31 +1,80 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+  const [title, setTitle] = useState("");
+  const [url, setUrl] = useState("");
+  const [techs, setTechs] = useState([]);
+
+  useEffect(() => {
+    async function loadRepositories() {
+      const response = await api.get("/repositories");
+
+      setRepositories(response.data);
+    }
+
+    loadRepositories();
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+      const repository = {
+        title,
+        url,
+        techs,
+      };
+  
+      const response = await api.post('/repositories', repository);
+  
+      setRepositories([...repositories, response.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`/repositories/${id}`);
+    
+    setRepositories(repositories.filter(item => item.id !== id));
   }
 
   return (
-    <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+      <div className="container">
+        <input
+          type="text"
+          placeholder="Título do repositório"
+          value={title}
+          required
+          onChange={(event) => setTitle(event.target.value)}
+        />
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
-      </ul>
+        <input
+          type="text"
+          placeholder="Url do repositório"
+          value={url}
+          required
+          onChange={(event) => setUrl(event.target.value)}
+        />
 
-      <button onClick={handleAddRepository}>Adicionar</button>
-    </div>
-  );
-}
+        <input
+          type="text"
+          placeholder="Tecnologias do repositório"
+          value={techs}
+          required={true}
+          onChange={(event) => setTechs(event.target.value)}
+        />
+
+        <button onClick={handleAddRepository}>Adicionar</button>
+        <ul data-testid="repository-list">
+           {repositories.map((repository) => (
+              <li key={repository.id}>
+                {repository.title}
+                <button onClick={() => handleRemoveRepository(repository.id)}>
+                  Remover
+                </button>
+              </li>
+            ))}
+         
+        </ul>
+      </div>
+  )}
 
 export default App;
